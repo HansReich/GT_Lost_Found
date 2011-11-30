62e14235e8b46061ef2048caf86a84bfcf0ac0ab
Hans Reichenbach <HansReich25@gmail.com>
2011-11-29 22:13:50 -0500

    initial Android Project Code

---
+++ AndroidCode/src/edu/gatech/MobileApps/LostFound/GTLost_FoundActivity.java
@@ -0,0 +1,13 @@
+package edu.gatech.MobileApps.LostFound;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GTLost_FoundActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
