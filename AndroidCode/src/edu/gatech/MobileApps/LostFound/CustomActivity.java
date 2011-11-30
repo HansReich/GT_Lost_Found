79e5c110fe3885a975039a3106dbf83b6fbaf7a0
Hans Reichenbach <HansReich25@gmail.com>
2011-11-29 22:26:53 -0500

    added generalized fouls

---
+++ AndroidCode/src/edu/gatech/MobileApps/LostFound/CustomActivity.java
@@ -0,0 +1,19 @@
+package edu.gatech.MobileApps.LostFound;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+public class CustomActivity extends Activity {
+private static String TAG = "CustomActivity";
+	
+	/** called when the activity is first made*/
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+	
+	public SharedPreferences getPreferences() {
+		LostFoundApp app = (LostFoundApp)getApplication();
+		return app.getPreferences();
+	}
+}
