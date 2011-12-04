c473d746fa4fab1a60e29cdeb37e177704ec61dd
Hans Reichenbach <HansReich25@gmail.com>
2011-12-04 15:43:40 -0500

    still tweaking layouts, added the new mpeg video

--- AndroidCode/src/com/lostandfound/ReportFoundActivity.java
+++ AndroidCode/src/com/lostandfound/ReportFoundActivity.java
@@ -3,10 +3,14 @@ package com.lostandfound;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TimePicker;
 
 public class ReportFoundActivity extends CustomActivity implements View.OnClickListener{
-	EditText foundlocation, item, date, time, picklocation, email, phone, description;
+	EditText foundlocation, item, picklocation, email, phone, description;
+	DatePicker date;
+	TimePicker time;
 	Button submit;
 	
 	/* (non-Javadoc)
@@ -24,8 +28,8 @@ public class ReportFoundActivity extends CustomActivity implements View.OnClickL
 	private void initializeVars(){
 		foundlocation = (EditText)findViewById(R.id.edfoundlocation);
 		item = (EditText) findViewById(R.id.editem);
-		date = (EditText) findViewById(R.id.eddate);
-		time = (EditText) findViewById(R.id.edtime);
+		date = (DatePicker) findViewById(R.id.eddate);
+		time = (TimePicker) findViewById(R.id.edtime);
 		picklocation = (EditText) findViewById(R.id.edpickuplocation);
 		email = (EditText) findViewById(R.id.edemail);
 		phone = (EditText) findViewById(R.id.edphone);
