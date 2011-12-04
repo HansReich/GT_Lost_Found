58006265679c4c616b9d7d3f58d043d637039e65
Hans Reichenbach <HansReich25@gmail.com>
2011-12-03 22:33:19 -0500

    added empty listeners for the new buttons

--- AndroidCode/src/com/lostandfound/MainMenuActivity.java
+++ AndroidCode/src/com/lostandfound/MainMenuActivity.java
@@ -1,12 +1,19 @@
 package com.lostandfound;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 /**
  * The MainMenuActivity.
  */
 public class MainMenuActivity extends CustomActivity {
+    /** constants needed*/
     public static final String TAG = "MainMenuActivity";
+    
+    /** views on the screen*/
+    Button reportFound, searchLost;
     
     /**
      * Called when the activity is first created.
@@ -20,5 +27,25 @@ public class MainMenuActivity extends CustomActivity {
         setContentView(R.layout.mainmenu);
         
         Log.d(TAG, "Creating main menu activity");
+        
+        //set up the button listeners
+        reportFound = (Button) findViewById(R.id.FoundButton);
+        searchLost = (Button) findViewById(R.id.LostButton);
+        reportFound.setOnClickListener(myFoundListener);
+        searchLost.setOnClickListener(myLostListener);
     }
+    
+    /** what to do when reportFound button is clicked*/
+    private OnClickListener myFoundListener = new OnClickListener() {
+        public void onClick(View v) {
+            //TODO
+        }
+    };
+    
+    /** what to do when searchLost button is clicked*/
+    private OnClickListener myLostListener = new OnClickListener() {
+        public void onClick(View v) {
+            //TODO
+        }
+    };
 }
