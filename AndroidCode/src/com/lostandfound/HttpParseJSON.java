c224bfcda086306439c6b52d5777dce41c600297
Hans Reichenbach <HansReich25@gmail.com>
2011-12-04 18:06:16 -0500

    removed all the commented out code

--- AndroidCode/src/com/lostandfound/HttpParseJSON.java
+++ AndroidCode/src/com/lostandfound/HttpParseJSON.java
@@ -13,13 +13,11 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.app.Activity;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class HttpParseJSON extends Activity{
+public class HttpParseJSON extends CustomActivity{
 	TextView httpStuff;
 	HttpClient client;
 	JSONObject json;
@@ -30,10 +28,10 @@ public class HttpParseJSON extends Activity{
 	 * @see android.app.Activity#onCreate(android.os.Bundle)
 	 */
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.http);
+		setContentView(R.layout.searchresults);
 		httpStuff = (TextView)findViewById(R.id.httpinfo);
 		client = new DefaultHttpClient();
 		try {
@@ -66,8 +64,8 @@ public class HttpParseJSON extends Activity{
 				if(timeline.isNull(i) == false){
 					JSONObject item = timeline.getJSONObject(i);
 					String pubdes = item.getString("publicdes");
-					String phone = item.getString("email");
-					String email = item.getString("phone");
+					String phone = item.getString("phone");
+					String email = item.getString("email");
 					String pickup = item.getString("pickup");
 					httpStuff.append("item " + i + "\n");
 					httpStuff.append("Description: ");
@@ -84,67 +82,9 @@ public class HttpParseJSON extends Activity{
 					httpStuff.append("\n");
 				}
 			}
-		}else{
+		} else {
 			Toast.makeText(HttpParseJSON.this, "error", Toast.LENGTH_LONG);
 		}
 	}
-		/*  public JSONObject lastlostfound(String username)throws ClientProtocolException, IOException, JSONException{
-			StringBuilder url = new StringBuilder(URL);
-			url.append(username);
-			
-			HttpGet get = new HttpGet(url.toString());
-			HttpResponse r = client.execute(get);
-			int status = r.getStatusLine().getStatusCode();
-			if(status == 200){
-				HttpEntity e = r.getEntity();
-				String data = EntityUtils.toString(e);
-				JSONArray timeline = new JSONArray(data);
-				for(int i=0; i<timeline.length();i++){
-					if(timeline.isNull(i) == false){
-						JSONObject item = timeline.getJSONObject(i);
-						String pubdes = item.getString("publicdes");
-						String pickup = item.getString("pickup");
-					}
-				}
-				JSONObject last = timeline.getJSONObject(1);
-				return last;
-			}else{
-				Toast.makeText(HttpParseJSON.this, "error", Toast.LENGTH_LONG);
-				return null;
-			}
-		}*/
-		
-		/*public class Read extends AsyncTask<String, Integer, String>{
-
-			@Override
-			protected String doInBackground(String... params) {
-				// TODO Auto-generated method stub
-				try {
-					json = lastlostfound("");
-					return json.getString(params[0]);
-				} catch (ClientProtocolException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (JSONException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				return null;
-			}*/
-
-			/* (non-Javadoc)
-			 * @see android.os.AsyncTask#onPostExecute(java.lang.Object)
-			 */
-			//@Override
-			/*protected void onPostExecute(String result) {
-				// TODO Auto-generated method stub
-				//super.onPostExecute(result);
-				httpStuff.append(result);
-				//httpStuff.setText(result);
-			}*/
-		//}
 
 	}
