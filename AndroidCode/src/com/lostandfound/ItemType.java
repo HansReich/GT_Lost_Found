2b28da0bff7a367c3528d3057b97ecdd2a2f9461
Hans Reichenbach <HansReich25@gmail.com>
2011-12-03 20:22:18 -0500

    added back in my files from before

---
+++ AndroidCode/src/com/lostandfound/ItemType.java
@@ -0,0 +1,15 @@
+package com.lostandfound;
+
+public enum ItemType {
+    SMARTPHONE,
+    LAPTOP,
+    BACKPACK,
+    UMBRELLA,
+    JACKET,
+    HAT,
+    WALLET,
+    BUZZCARD,
+    SHOES,
+    PURSE,
+    MISCELLANEOUS
+}
---
+++ AndroidCode/src/com/lostandfound/LostFoundApp.java
@@ -0,0 +1,41 @@
+package com.lostandfound;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class LostFoundApp extends Application {
+	private static String TAG = "LostFound App";
+	/* The instance of this Application. */
+	private static LostFoundApp instance; 
+	
+	/* The SharedPreferences provider that the application will use. */
+	private SharedPreferences preferences;
+	
+	/* Set up our singleton. */
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		Log.v(TAG, "ON CREATE");
+		
+		instance = this;
+		preferences = getSharedPreferences(Common.MAIN_PREFERENCES,
+				Context.MODE_PRIVATE);
+	}
+	
+	public static LostFoundApp getInstance() {
+		return instance;
+	}
+	
+	public static void setInstance(LostFoundApp instance) {
+		LostFoundApp.instance = instance;
+	}
+
+	public SharedPreferences getPreferences() {
+		return preferences;
+	}
+
+	public void setPreferences(SharedPreferences preferences) {
+		this.preferences = preferences;
+	}
+}
